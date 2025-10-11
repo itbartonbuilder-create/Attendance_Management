62c439dab8607c9e43b2365bf788c517e7251b6a
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-10-11 11:39:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";   
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Workers from "./pages/Workers";
+import Reports from "./pages/Reports";
 import Attendance from "./pages/Attendance";
 import Profile from "./pages/Profile";
-import Reports from "./pages/Reports"; 
-
-function Layout({ children }) {
-  const location = useLocation();
-  const hideLayout = location.pathname === "/" || location.pathname === "/register";
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-      }}
-    >
-      {!hideLayout && <Navbar />}
-      
-      <main style={{ flex: 1 }}>{children}</main>
-      {!hideLayout && <Footer />}
-    </div>
-  );
-}
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/workers" element={<Workers />} />
-          <Route path="/attendance" element={<Attendance />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/reports" element={<Reports />} />
-
-        </Routes>
-      </Layout>
+      <Navbar />
+      <Routes>
+       <Route path="/" element={<Login />} /> 
+        
+     
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/workers" element={<Workers />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
