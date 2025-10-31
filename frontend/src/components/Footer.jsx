20c58eecf6581413f487f7c0bae8d87ed3f259d2
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-10-31 11:29:37 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,14 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer
-      style={{
-        background: "#2C3E50",
-        color: "white",
-        textAlign: "center",
-        padding: "15px 10px",
-      }}
-    >
+    <footer>
       <p>© 2025 Bartons Builders Limited. All rights reserved.</p>
     </footer>
   );
