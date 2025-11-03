379a9d1600dae49c29fdeb889c1c048dcaaa0d33
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-11-03 12:24:50 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Footer() {
+    const location = useLocation();
+
+  if (location.pathname === "/" || location.pathname === "/login") return null;
   return (
     <footer>
       <p>© 2025 Bartons Builders Limited. All rights reserved.</p>
