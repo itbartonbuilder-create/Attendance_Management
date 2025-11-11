d1de74e9f5b0bf0b59e9a780fcff6ab21f04cdbe
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-11-11 11:40:32 +0530

    Update Attendance.js

--- backend/models/Attendance.js
+++ backend/models/Attendance.js
@@ -7,15 +7,21 @@ const AttendanceSchema = new mongoose.Schema({
     {
       workerId: { type: mongoose.Schema.Types.ObjectId, ref: "Worker", required: true },
       name: { type: String, required: true },
-      roleType: { type: String },
+      roleType: { type: String, required: true },
       role: { type: String, required: true },
       status: { type: String, enum: ["Present", "Absent", "Leave"], required: true },
       hoursWorked: { type: Number, default: 0 },
-      overtimeHours: { type: Number, default: 0 }, // ✅ stores daily overtime
       salary: { type: Number, default: 0 },
+
+      leaveType: {
+        holiday: { type: Boolean, default: false },
+        accepted: { type: Boolean, default: false },
+      },
     },
   ],
 });
 
+
 AttendanceSchema.index({ date: 1, site: 1 }, { unique: true });
+
 export default mongoose.model("Attendance", AttendanceSchema);
