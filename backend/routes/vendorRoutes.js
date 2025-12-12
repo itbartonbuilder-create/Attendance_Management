81be22334c45c919ba5437639ac27384a59e9123
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-12 12:15:43 +0530

    Create vendorRoutes.js

---
+++ backend/routes/vendorRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { registerVendor, loginVendor } from "../controllers/vendorController.js";
+
+const router = express.Router();
+
+router.post("/register", registerVendor);
+router.post("/login", loginVendor);
+
+export default router;
