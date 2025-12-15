6fc92a560839a4a5766378fee6c51efdee9635f3
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-15 11:16:35 +0530

    Update vendorModel.js

--- backend/models/vendorModel.js
+++ backend/models/vendorModel.js
@@ -5,6 +5,33 @@ const vendorSchema = new mongoose.Schema(
     name: { type: String, required: true },
     companyName: { type: String },
     contactNo: { type: String, required: true, unique: true },
+
+    aadharNumber: {
+      type: String,
+      required: true,
+      length: 12,
+    },
+
+    panNumber: {
+      type: String,
+      required: true,
+    },
+
+    vendorType: {
+      type: String,
+      enum: ["supply", "work"],
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    gstNumber: {
+      type: String,
+    },
+
     password: { type: String, required: true },
   },
   { timestamps: true }
