bcdd0e249a9e5f1d23b6cfc7d1aa65eaf5c8b667
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-16 13:21:23 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,25 +1,29 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
-
-const uploadDir = "uploads";
-
-// uploads folder ensure
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    cb(null, Date.now() + ext);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "application/pdf"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image or pdf allowed"), false);
+  }
+};
+
 export const upload = multer({
   storage,
+  fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
