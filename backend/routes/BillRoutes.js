e7ee777ce79e1a3fe079b1731f494355171c0c6d
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-17 12:02:25 +0530

    Update BillRoutes.js

--- backend/routes/BillRoutes.js
+++ backend/routes/BillRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBill } from "../controllers/BillController.js";
+import { createBill, getBillsByManager } from "../controllers/BillController.js";
 import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.post("/create", upload.single("billFile"), createBill);
+router.get("/manager/:managerId", getBillsByManager);
 
 export default router;
