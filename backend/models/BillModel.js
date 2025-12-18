0d0cc58be6e667ddeaf7add24569bf26feb4883c
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-18 15:57:56 +0530

    Update BillModel.js

--- backend/models/BillModel.js
+++ backend/models/BillModel.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 const billSchema = new mongoose.Schema(
   {
-    workName: { type: String, required: true },
-    billNo: { type: String, required: true },
-    site: { type: String, required: true },
-    sentTo: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
-    amount: { type: Number, required: true },
-    billDate: { type: Date, required: true }, 
-    billFile: { type: String },
+    workName: String,
+    billNo: String,
+    site: String,
+    sentTo: String,
+    amount: Number,
+    billDate: String,
+    billFile: String,
   },
   { timestamps: true }
 );
