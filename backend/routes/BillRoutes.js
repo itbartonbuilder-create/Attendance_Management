7917c80442197208c5b0627d230f5e6527c452de
BartonsBuilders <it.bartonbuilder@gmail.com>
2025-12-24 11:03:11 +0530

    Update BillRoutes.js

--- backend/routes/BillRoutes.js
+++ backend/routes/BillRoutes.js
@@ -2,29 +2,41 @@ import express from "express";
 import mongoose from "mongoose";
 import Bill from "../models/BillModel.js";
 import "../models/User.js";
+import "../models/vendorModel.js";
 import { createBill } from "../controllers/BillController.js";
 import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
-
 router.post("/create", upload.single("billFile"), createBill);
+
 
 router.get("/", async (req, res) => {
   try {
-    const { role, site, manager } = req.query;
+    const { role, site, manager, vendor } = req.query;
+
     let filter = {};
 
+    // MANAGER VIEW
     if (role === "manager") {
       if (site) filter.site = site;
+
       if (manager && mongoose.Types.ObjectId.isValid(manager)) {
-        filter.sentTo = new mongoose.Types.ObjectId(manager); // ✅ use 'new'
+        filter.sentTo = new mongoose.Types.ObjectId(manager);
+      }
+    }
+
+
+    if (role === "vendor") {
+      if (vendor && mongoose.Types.ObjectId.isValid(vendor)) {
+        filter.vendor = new mongoose.Types.ObjectId(vendor);
       }
     }
 
     const bills = await Bill.find(filter)
       .sort({ createdAt: -1 })
-      .populate("sentTo", "name"); // populate only name
+      .populate("sentTo", "name email")
+      .populate("vendor", "name companyName contactNo");
 
     res.status(200).json(bills);
   } catch (error) {
@@ -32,4 +44,5 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
 export default router;
