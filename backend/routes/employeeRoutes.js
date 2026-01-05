1b5f51305c5a1ae69ee234b770886367c495b2c6
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-01-05 14:01:01 +0530

    Create employeeRoutes.js

---
+++ backend/routes/employeeRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { upload } from "../middlewares/upload.js";
+import {
+  addEmployee,
+  getEmployees,
+  deleteEmployee,
+} from "../controllers/employeeController.js";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  upload.fields([
+    { name: "aadhaar", maxCount: 1 },
+    { name: "pan", maxCount: 1 },
+  ]),
+  addEmployee
+);
+
+router.get("/", getEmployees);
+router.delete("/:id", deleteEmployee);
+
+export default router;
