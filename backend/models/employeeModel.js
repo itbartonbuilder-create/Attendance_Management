8d968307451d20db2c94cb5bccb866d5e6c2b3da
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-01-05 14:48:04 +0530

    Update employeeModel.js

--- backend/models/employeeModel.js
+++ backend/models/employeeModel.js
@@ -3,21 +3,12 @@ import mongoose from "mongoose";
 const employeeSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-
-    role: { type: String, required: true }, // Engineer, Supervisor etc
-
+    role: { type: String, required: true }, 
     site: { type: String, required: true },
-
     contactNo: { type: String, required: true },
-
-    aadhaarNumber: { type: String, required: true },
-
-    panNumber: { type: String, required: true },
-
     salary: { type: Number, required: true },
-
     aadhaarDoc: { type: String }, 
-    panDoc: { type: String },     
+    panDoc: { type: String }, 
   },
   { timestamps: true }
 );
