648c20f3d0709ae6106cdd0a9c37e4b391e6d415
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-01-06 12:32:58 +0530

    Update Manager.js

--- backend/models/Manager.js
+++ backend/models/Manager.js
@@ -5,6 +5,16 @@ const ManagerSchema = new mongoose.Schema({
   email: { type: String, required: true },
   contactNo: { type: String, required: true },
   site: { type: String, required: true },
-});
+
+  // Optional documents
+  aadhaarDoc: {
+    url: { type: String },
+    public_id: { type: String },
+  },
+  panDoc: {
+    url: { type: String },
+    public_id: { type: String },
+  },
+}, { timestamps: true }); 
 
 export default mongoose.model("Manager", ManagerSchema);
