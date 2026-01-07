1e4c18d56d1528bfeed3457f091b9dccb0b9e6c5
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-01-07 10:57:29 +0530

    Update BillRoutes.js

--- backend/routes/BillRoutes.js
+++ backend/routes/BillRoutes.js
@@ -1,35 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
 import Bill from "../models/BillModel.js";
-import "../models/User.js";
-import "../models/vendorModel.js";
 import { createBill } from "../controllers/BillController.js";
-import { upload } from "../middleware/upload.js";
+import { uploadBill } from "../middleware/upload.js";
 
 const router = express.Router();
 
-router.post("/create", upload.single("billFile"), createBill);
+router.post("/create", uploadBill.single("billFile"), createBill);
 
 
 router.get("/", async (req, res) => {
   try {
     const { role, site, manager, vendor } = req.query;
-
     let filter = {};
 
-    // MANAGER VIEW
     if (role === "manager") {
       if (site) filter.site = site;
-
       if (manager && mongoose.Types.ObjectId.isValid(manager)) {
-        filter.sentTo = new mongoose.Types.ObjectId(manager);
+        filter.sentTo = manager;
       }
     }
 
-
     if (role === "vendor") {
       if (vendor && mongoose.Types.ObjectId.isValid(vendor)) {
-        filter.vendor = new mongoose.Types.ObjectId(vendor);
+        filter.vendor = vendor;
       }
     }
 
@@ -40,7 +34,6 @@ router.get("/", async (req, res) => {
 
     res.status(200).json(bills);
   } catch (error) {
-    console.error("Error fetching bills:", error.message);
     res.status(500).json({ message: error.message });
   }
 });
