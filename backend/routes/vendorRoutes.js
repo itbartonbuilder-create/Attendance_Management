954d80a5fa089e936174afbe9323a83c1d6e4c79
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-01-17 11:17:19 +0530

    Update vendorRoutes.js

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -3,7 +3,6 @@ import {
   registerVendor,
   loginVendor,
   getAllVendors,
-  approveVendor,
 } from "../controllers/vendorController.js";
 
 const router = express.Router();
@@ -11,6 +10,5 @@ const router = express.Router();
 router.post("/register", registerVendor);
 router.post("/login", loginVendor);
 router.get("/", getAllVendors);
-router.put("/approve/:id", approveVendor);
 
 export default router;
