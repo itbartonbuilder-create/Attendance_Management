3d31667aef71c4de461a658edd269dc422b93a2d
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-02-04 14:42:52 +0530

    Update Stock.js

--- backend/models/Stock.js
+++ backend/models/Stock.js
@@ -2,40 +2,13 @@ import mongoose from "mongoose";
 
 const stockSchema = new mongoose.Schema(
   {
-    site: {
-      type: String,
-      required: true,
-    },
-
-    category: {
-      type: String,
-      required: true,
-    },
-
-    material: {
-      type: String,
-      required: true,
-    },
-
-    unit: {
-      type: String,
-      required: true,
-    },
-
-    totalStock: {
-      type: Number,
-      required: true,
-    },
-
-    usedStock: {
-      type: Number,
-      required: true,
-    },
-
-    remainingStock: {
-      type: Number,
-      required: true,
-    },
+    site: { type: String, required: true },
+    category: { type: String, required: true },
+    material: { type: String, required: true },
+    unit: { type: String, required: true },
+    totalStock: { type: Number, required: true },
+    usedStock: { type: Number, required: true },
+    remainingStock: { type: Number, required: true },
   },
   { timestamps: true }
 );
