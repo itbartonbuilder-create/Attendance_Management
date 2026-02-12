0fca50e9f322c3a51ea5896af2c8beccc31a94ec
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-02-12 13:01:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -565,14 +565,13 @@ function Login() {
                   />
                 </>
               )}
-{vendorMode === "login" && (
-           <ReCAPTCHA
+
+              <ReCAPTCHA
   sitekey="6Le4zmcsAAAAAIT4l3JLSicblw3j-KmCu6Lllxdz"
   onChange={(token) => setCaptchaToken(token)}
 />
-  )}
               <div style={{ display: "flex", gap: "10px" }}>
-     
+  
                 <button type="submit" style={{ ...buttonStyle, flex: 1 }}>
                   {vendorMode === "login" ? "Login" : "Register"}
                 </button>
