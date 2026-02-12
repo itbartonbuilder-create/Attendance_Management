2f8eeabe4c15159fad3108db34c38448f8143627
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-02-12 14:46:37 +0530

    Update Managers.jsx

--- frontend/src/pages/Managers.jsx
+++ frontend/src/pages/Managers.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "../App.css";
 
 function Managers() {
-  const defaultSites = ["Bangalore", "Japuriya", "Vaishali", "Faridabad", "Other"];
+  const defaultSites = ["Bangalore", "Japuriya", "Vaishali", "Faridabad","jim corbett", "Other"];
 
   const [user, setUser] = useState(null);
   const [managers, setManagers] = useState([]);
