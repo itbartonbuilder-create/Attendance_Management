be8fe1e473ac9a3dbbc1f39209bbc407b213c402
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-02-20 14:32:26 +0530

    Update DailyReport.js

--- backend/models/DailyReport.js
+++ backend/models/DailyReport.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const dailyReportSchema = new mongoose.Schema({
   date: String,
-  siteId: { type: String, required: true }, 
-
+  siteId: { type: String, required: true },
   morningText: { type: String, default: "" },
   eveningText: { type: String, default: "" },
-
   morningPhotos: { type: [String], default: [] },
   eveningPhotos: { type: [String], default: [] },
-
   createdAt: { type: Date, default: Date.now },
 });
 
