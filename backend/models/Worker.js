914581e7f417290cae733f22b0542105e33c7907
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-03-21 13:57:43 +0530

    Update Worker.js

--- backend/models/Worker.js
+++ backend/models/Worker.js
@@ -21,7 +21,45 @@ const WorkerSchema = new mongoose.Schema({
   locationName: {
   type: String,
   default: null
-}
+},
+  import mongoose from "mongoose";
+
+const WorkerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  roleType: { type: String, required: true },
+  role: { type: String, required: true },
+  site: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  perDaySalary: { type: Number, required: true },
+   latitude: {
+    type: Number,
+    default: null
+  },
+  longitude: {
+    type: Number,
+    default: null
+  },
+  lastLocationUpdate: {
+    type: Date
+  },
+  locationName: {
+  type: String,
+  default: null
+},
+  locationHistory: [
+    {
+      latitude: Number,
+      longitude: Number,
+      locationName: String,
+      date: String,
+      time: Date,
+    }
+  ]
+   
+});
+
+export default mongoose.model("Worker", WorkerSchema);
+
    
 });
 
