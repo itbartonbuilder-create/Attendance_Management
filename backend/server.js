c5e5776f1ae16a3e843b8a8e952e87d707f2b583
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-04-01 12:08:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,6 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-
-
 import connectDB from "./config/db.js";
 import path from "path";
 import { fileURLToPath } from "url";
