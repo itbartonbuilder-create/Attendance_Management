b10483fc2e82e150515f10dc00e1c813b10d0b19
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-04-17 14:56:58 +0530

    Update AdminBills.jsx

--- frontend/src/pages/AdminBills.jsx
+++ frontend/src/pages/AdminBills.jsx
@@ -5,7 +5,6 @@ import * as XLSX from "xlsx";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 
-
 const AdminBills = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const [bills, setBills] = useState([]);
