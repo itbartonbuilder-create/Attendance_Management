34c700187c5729f276a9bb396329c50d7c7ef862
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-04-22 15:27:24 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { approveVendor } from "../controllers/adminController.js";
 
 const router = express.Router();
-
 router.put("/approve-vendor/:id", approveVendor);
 
 export default router;
