c665d883e89a7ff502a123406f99c1840166313f
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-04-27 15:14:10 +0530

    Update VendorSubmitBill.jsx

--- frontend/src/pages/VendorSubmitBill.jsx
+++ frontend/src/pages/VendorSubmitBill.jsx
@@ -4,6 +4,7 @@ import BillForm from "../components/BillForm";
 const VendorSubmitBill = () => {
   const user = JSON.parse(localStorage.getItem("user"));
 
+  
   return (
     <div style={{ paddingTop: "90px" }}>
       <BillForm vendorId={user._id} />
