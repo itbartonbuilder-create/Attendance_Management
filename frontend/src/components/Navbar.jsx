d26c251c3ee50270822dde44c813d251ff457bb2
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-05-07 11:24:05 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import logo from "../assets/logo.png";
 function Navbar() {
   const [workersDropdown, setWorkersDropdown] = useState(false);
   // const [managementDropdown, setManagementDropdown] = useState(false);
-
   
   const [user, setUser] = useState(null);
   const [menuOpen, setMenuOpen] = useState(false);
