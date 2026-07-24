537c6f0dbbc6b6faad02c876b467f7ac0e848886
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-07-24 12:32:51 +0530

    Create Voucher.js

---
+++ backend/models/Voucher.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const voucherSchema = new mongoose.Schema({
+  voucherNo: { type: String, required: true, unique: true },
+  payableTo: { type: String, required: true },
+  particulars: { type: String, required: true },
+  paymentMode: { type: String, required: true },
+  amount: { type: Number, required: true },
+  amountInWords: { type: String, required: true },
+  site: { type: String, required: true },          
+  createdByName: { type: String, required: true },  
+  createdByUserId: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Voucher = mongoose.model("Voucher", voucherSchema);
+export default Voucher;
