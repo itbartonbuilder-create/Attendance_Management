376cc849de6bd9b5596ef7497115250c937af306
BartonsBuilders <it.bartonbuilder@gmail.com>
2026-07-24 15:23:52 +0530

    Update voucher.js

--- backend/routes/voucher.js
+++ backend/routes/voucher.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createVoucher, getVouchers, getNextVoucherNumber } from "../controllers/VoucherController.js";
+import { uploadVoucher } from "../middleware/upload.js";
 
 const router = express.Router();
 
-
-router.post("/create", createVoucher);
+router.post("/create", uploadVoucher.single("proof"), createVoucher);
 
 router.get("/next-number", getNextVoucherNumber);
 
